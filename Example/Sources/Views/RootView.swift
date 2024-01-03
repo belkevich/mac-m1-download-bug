fdeba18f45c51c170b4b3cf31b488c24cbc49d1e
Oleksii Belkevych <belkevich.alexey@gmail.com>
2024-01-03 19:18:39 +0200

    Initial commit

---
+++ Example/Sources/Views/RootView.swift
@@ -0,0 +1,21 @@
+//
+//  RootView.swift
+//  Example
+//
+//  Created by Megogo on 27.12.2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("Example")
+            .font(.system(size: 38, weight: .bold))
+            .foregroundColor(.accentColor)
+    }
+}
+
+#Preview {
+    RootView()
+}
---
+++ ExampleTests/ExampleTests.swift
@@ -0,0 +1,21 @@
+//
+//  ExampleTests.swift
+//  ExampleTests
+//
+//  Created by Megogo on 27.12.2023.
+//
+
+import XCTest
+@testable import Example
+
+final class ExampleTests: XCTestCase {
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+        XCTFail("Add some tests")
+    }
+}
